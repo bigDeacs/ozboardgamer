ad4f58d265ff7b53e90c196977877a7ad4b6ec9b
unknown <brent.deacon@AP-TMMGR01.iceenterprise.com>
2016-03-11 11:32:51 +1000

    Add theme

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -70,7 +70,7 @@ class PostController extends Controller
         }
         $post->games()->sync($currentGames);
 
-        return redirect('/posts');
+        return redirect('/admin/posts');
     }
 
     /**
