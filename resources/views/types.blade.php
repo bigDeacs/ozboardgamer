5d49f3166e4c2e8d5b6128b85b6f0794bdd3e369
unknown <brent.deacon@AP-TMMGR01.iceenterprise.com>
2016-03-15 16:34:05 +1000

    Finish

--- resources/views/types.blade.php
+++ resources/views/types.blade.php
@@ -20,7 +20,7 @@
 			    	@foreach($types as $type)
 						<div class="col-sm-3 col-xs-12">
 					    	<a href="/games/{{ $type->slug }}">
-				    			<img src="{{ $type->games()->first()->image }}" class="img-responsive" />
+				    			<img src="{{ $type->games->first()->image }}" class="img-responsive" />
 				    		</a>
 					    	<p><a href="/games/{{ $type->slug }}">{!! $type->name !!}</a></p>
 						</div>
@@ -31,18 +31,18 @@
 				@foreach($games as $game)
 					<div class="row">
 						<div class="col-sm-2 col-xs-12">
-							<a href="/games/{{ $game->types()->first()->slug }}/{{ $game->slug }}">
+							<a href="/games/{{ $game->types->first()->slug }}/{{ $game->slug }}">
 					    		<img src="{{ $game->image }}" class="img-responsive" />
 					    	</a>
 					    </div>
 						<div class="col-sm-8 col-xs-12">
-					    	<a href="/games/{{ $game->types()->first()->slug }}/{{ $game->slug }}">
+					    	<a href="/games/{{ $game->types->first()->slug }}/{{ $game->slug }}">
 					    		{!! $game->name !!}
 					    	</a>
 					    	<p>{!! str_limit(strip_tags($game->description), $limit = 100, $end = '...') !!}</p>
 						</div>
 						<div class="col-sm-2 col-xs-12 text-center">
-					    	<a href="/games/{{ $game->types()->first()->slug }}/{{ $game->slug }}" class="btn btn-warning">
+					    	<a href="/games/{{ $game->types->first()->slug }}/{{ $game->slug }}" class="btn btn-warning">
 					    		Find Out More
 					    	</a>
 						</div>
