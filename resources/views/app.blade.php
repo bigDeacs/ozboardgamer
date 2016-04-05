ebe05a1c3e2b050845605cc8fd338b8a25a7a2fd
unknown <brent.deacon@AP-TMMGR01.iceenterprise.com>
2016-04-05 13:44:23 +1000

    add og tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,6 +26,9 @@
     <meta name="msapplication-TileColor" content="#603cba">
     <meta name="msapplication-TileImage" content="/mstile-144x144.png">
     <meta name="theme-color" content="#222222">
+    <meta property="og:title"              content="Find the latest News, Reviews and More with Oz Board Gamer" />
+    <meta property="og:description"        content="Want to know all the latest and greatest about Board Games? We have News, Reviews and much more!" />
+    <meta property="og:image"              content="/favicon-194x194.png" />
     @yield('meta')
     @yield('head')
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
