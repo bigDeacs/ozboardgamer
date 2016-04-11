d2fbd5d8fc108af3ecec1067c6d2420be43d0a94
Brent <brentdeacon23@gmail.com>
2016-04-11 20:17:12 +1000

    remove search

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,21 +10,6 @@
 
 @section('content')
 <!-- Header -->
-<div>
-    <script>
-      (function() {
-        var cx = '006771009055272743161:pdppriexou4';
-        var gcse = document.createElement('script');
-        gcse.type = 'text/javascript';
-        gcse.async = true;
-        gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-            '//cse.google.com/cse.js?cx=' + cx;
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(gcse, s);
-      })();
-    </script>
-    <gcse:search></gcse:search>
-</div>
     <!-- Header Carousel -->
     <header id="myCarousel" class="carousel slide hidden-xs">
         @unless($featured->isEmpty())
