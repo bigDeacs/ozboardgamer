a974431c1e73ef994b816babec62014ddfe0b51c
unknown <brent.deacon@AP-TMMGR01.iceenterprise.com>
2016-04-12 14:14:36 +1000

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -11,8 +11,9 @@ var client = algoliasearch('LAC06A9QLK', 'fdfd933ae74a447ae896c1279229c83b');
               console.log(suggestion);
               return '<a href="'+suggestion.slug+'"><div class="row">'+
                 '<div class="col-xs-2"><img src="'+suggestion.thumb+'" class="img-responsive" /></div>'+
-                '<div class="col-xs-8">'+suggestion._highlightResult.name.value+'</div>'+
-                '<div class="col-xs-2"><img src="/img/'+Math.ceil(parseInt(suggestion._highlightResult.rating.value))+'.png" class="img-responsive" /></div>'+
+                '<div class="col-xs-6">'+suggestion._highlightResult.name.value+'</div>'+
+                '<div class="col-xs-2"><div class="text-center lead"><strong>'+Number(suggestion.rating).toFixed(1)+'/10</strong></div></div>'+
+                '<div class="col-xs-2"><img src="/img/'+Math.ceil(Number(suggestion.rating))+'.png" class="img-responsive" /></div>'+
               '</div></a>';              
             }
           }
