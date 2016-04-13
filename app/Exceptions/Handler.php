7f599f0760b6ec1e49710c8cea7775c8fabe02a7
unknown <brent.deacon@AP-TMMGR01.iceenterprise.com>
2016-04-13 11:49:39 +1000

    add error page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,8 @@
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException as ModelNotFoundException;
+
 
 class Handler extends ExceptionHandler {
 
@@ -36,6 +38,12 @@ class Handler extends ExceptionHandler {
 	 */
 	public function render($request, Exception $e)
 	{
+		//add this to the render function
+		if ($e instanceof ModelNotFoundException)
+        {
+            return response()->view('errors.'.'404');
+        }
+        
 		return parent::render($request, $e);
 	}
 
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,35 @@
+@extends('app')
+
+@section('meta')
+    <title>Sorry, we couldn't find anything!</title>
+    <meta name="description" content="Want to know all the latest and greatest about Board Games? We have News, Reviews and much more!">
+@endsection
+
+@section('head')
+@endsection
+
+@section('content')
+	<!-- Page Content -->
+	<div class="container">
+	    <div class="row">
+            <div class="col-xs-12">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h3>Uh Oh!</h3>
+                       	Seems like you found a crack in our wall! We couldn't find what you were looking for, why don't you take a look at other parts of our site!
+                       	<div class="row">
+                       		<div class="col-xs-12 text-center">
+		                       	<img src="img/code.png" class="img-responsive" />
+		                    </div>
+		                </div>
+                        <hr />
+                    </div>
+                </div>
+        	</div>
+    	</div>
+    </div>
+    <!-- /.banner -->
+@endsection
+
+@section('scripts')
+@endsection
