3261ccdc8a7c97bf3cf70552e8a3c40dbd67acdf
unknown <brent.deacon@AP-TMMGR01.iceenterprise.com>
2016-04-26 17:02:32 +1000

    pagination

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -25,7 +25,7 @@
 			<ol class="breadcrumb breadcrumb-arrow">
 				<li><a href="/"><i class="glyphicon glyphicon-home"></i></a></li>
 				<li class="hidden-xs"><a href="/games">Games</a></li>
-				<li><a href="/games/{{ $game->types()->first()->slug }}">{{ $game->types()->first()->name }}</a></li>
+				<li><a href="/games/{{ $game->types()->first()->slug }}?page=1&sort=name-asc">{{ $game->types()->first()->name }}</a></li>
 				<li class="active"><span>{{ $game->name }}</span></li>
 			</ol>
 		</div>
