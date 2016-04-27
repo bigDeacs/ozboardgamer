9be506bfc419a6c53b0c8307e50792f677825025
unknown <brent.deacon@AP-TMMGR01.iceenterprise.com>
2016-04-27 16:38:11 +1000

    Fix to top

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -150,7 +150,7 @@
       
   	@yield('content')
 
-    <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left"><span class="glyphicon glyphicon-chevron-up"></span></a>
+    <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left" style="z-index: 100;"><span class="glyphicon glyphicon-chevron-up"></span></a>
 
   	<!-- Footer -->
     <footer class="navbar-inverse navbar-footer">
