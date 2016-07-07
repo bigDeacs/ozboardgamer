15c2dc359abfa5ecce1493fe8bc596143e981c2c
unknown <brent.deacon@AP-TMMGR01.iceenterprise.com>
2016-07-07 16:48:37 +1000

    profile

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -104,7 +104,7 @@
                             <li id="loginFB">
                                 @if(Session::has('name'))
                                     <div class="btn-group">
-                                      <a style="padding: 10px;font-weight: bold;" href="/users/{{ str_slug(Session::get('name')) }}?page=1&sort=name-asc" class="btn btn-primary" title="View Profile">Welcome, {{ strtok(Session::get('name'), " ") }}</a>
+                                      <a style="padding: 10px;font-weight: bold;" href="/users/{{ str_slug(Session::get('name')) }}?page=1&sort=name-asc" class="btn btn-primary" title="View Profile"><i class="fa fa-user"></i> Welcome, {{ strtok(Session::get('name'), " ") }}</a>
                                       <a style="padding: 10px;" href="/facebook/logout" class="btn btn-danger" title="Log Out"><i class="fa fa-sign-out"></i></a>                                    
                                     </div>                                    
                                 @else
