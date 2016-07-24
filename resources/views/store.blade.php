671ef0e4ac24aa94027880ee0d15619f0ec6abf8
unknown <brent.deacon@AP-TMMGR01.iceenterprise.com>
2016-07-25 09:52:03 +1000

    fix issue

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -39,10 +39,37 @@
 			      			<p><a href="{{ $store->link }}" target="_blank">Go To Site</a></p>
 			      		</div>
 			      		<div class="col-sm-6 col-xs-12">
-			      			<p>{{ $store->hours }}</p>
+			      			<p>{!! $store->hours !!}</p>
 			      		</div>
 			      	</div>
 			      	<div id="map"></div>
+			      	<script>
+						function initMap() {
+						  var map = new google.maps.Map(document.getElementById('map'), {
+						    zoom: 14,
+						    center: {lat: 59.323, lng: 18.010}
+						  });
+
+						  var layer = new google.maps.visualization.DynamicMapsEngineLayer({
+						    layerId: '06673056454046135537-08896501997766553811',
+						    map: map,
+						    suppressInfoWindows: true,
+						    clickable: true
+						  });
+
+						  layer.addListener('mouseover', function(event) {
+						    var style = layer.getFeatureStyle(event.featureId);
+						    style.fillColor = colors[event.featureId - 1];
+						    style.fillOpacity = '0.8';
+						  });
+
+						  layer.addListener('mouseout', function(event) {
+						    layer.getFeatureStyle(event.featureId).resetAll();
+						  });
+						}
+
+						var colors = ['red', 'blue', 'yellow', 'green'];
+				    </script>
 			      </div>
 			    </div>
 
@@ -66,33 +93,6 @@
 @endsection
 
 @section('scripts')
-	{!! $store->scripts !!}
-	<script>
-		function initMap() {
-		  var map = new google.maps.Map(document.getElementById('map'), {
-		    zoom: 14,
-		    center: {lat: 59.323, lng: 18.010}
-		  });
-
-		  var layer = new google.maps.visualization.DynamicMapsEngineLayer({
-		    layerId: '06673056454046135537-08896501997766553811',
-		    map: map,
-		    suppressInfoWindows: true,
-		    clickable: true
-		  });
-
-		  layer.addListener('mouseover', function(event) {
-		    var style = layer.getFeatureStyle(event.featureId);
-		    style.fillColor = colors[event.featureId - 1];
-		    style.fillOpacity = '0.8';
-		  });
-
-		  layer.addListener('mouseout', function(event) {
-		    layer.getFeatureStyle(event.featureId).resetAll();
-		  });
-		}
-
-		var colors = ['red', 'blue', 'yellow', 'green'];
-    </script>
+	{!! $store->scripts !!}	
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC52ck_jrm_AsqBe5CDNXIh7xrW6tmMyMQ&signed_in=true&libraries=visualization&callback=initMap"></script>
 @endsection
