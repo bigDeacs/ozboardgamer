b092a6d2e5a6a64669d99610404721531380beb9
unknown <brent.deacon@AP-TMMGR01.iceenterprise.com>
2016-07-25 09:45:24 +1000

    fix store page

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -7,6 +7,11 @@
 
 @section('head')
 	{!! $store->head !!}
+	<style>
+      #map {
+        height: 100%;
+      }
+    </style>
 @endsection
 
 @section('content')
@@ -25,6 +30,19 @@
 				<div class="row">
 			      <div class="col-xs-12">
 			      	<h1>{{ $store->name }}</h1>
+			      	<div class="row">
+			      		<div class="col-sm-6 col-xs-12">
+			      			<p>{{ $store->street }}</p>
+			      			<p>{{ $store->suburb }}, {{ $store->state }} {{ $store->postcode }}</p>
+			      			<p>{{ $store->phone }}</p>
+			      			<p><a href="mailto:{{ $store->email }}"></a></p>
+			      			<p><a href="{{ $store->link }}" target="_blank">Go To Site</a></p>
+			      		</div>
+			      		<div class="col-sm-6 col-xs-12">
+			      			<p>{{ $store->hours }}</p>
+			      		</div>
+			      	</div>
+			      	<div id="map"></div>
 			      </div>
 			    </div>
 
@@ -49,4 +67,32 @@
 
 @section('scripts')
 	{!! $store->scripts !!}
+	<script>
+		function initMap() {
+		  var map = new google.maps.Map(document.getElementById('map'), {
+		    zoom: 14,
+		    center: {lat: {{ $store->latitude }}, lng: {{ $store->longitude }}}
+		  });
+
+		  var layer = new google.maps.visualization.DynamicMapsEngineLayer({
+		    layerId: '06673056454046135537-08896501997766553811',
+		    map: map,
+		    suppressInfoWindows: true,
+		    clickable: true
+		  });
+
+		  layer.addListener('mouseover', function(event) {
+		    var style = layer.getFeatureStyle(event.featureId);
+		    style.fillColor = colors[event.featureId - 1];
+		    style.fillOpacity = '0.8';
+		  });
+
+		  layer.addListener('mouseout', function(event) {
+		    layer.getFeatureStyle(event.featureId).resetAll();
+		  });
+		}
+
+		var colors = ['red', 'blue', 'yellow', 'green'];
+    </script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC52ck_jrm_AsqBe5CDNXIh7xrW6tmMyMQ&signed_in=true&libraries=visualization&callback=initMap"></script>
 @endsection
