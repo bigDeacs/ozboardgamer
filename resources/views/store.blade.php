3630d2890b93d1bb3acd89fdd9623a51c85df4e7
unknown <brent.deacon@AP-TMMGR01.iceenterprise.com>
2016-07-25 14:01:37 +1000

    fix store page

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -33,13 +33,15 @@
 			      	<h1>{{ $store->name }}</h1>
 			      	<div class="row">
 			      		<div class="col-sm-6 col-xs-12">
+			      			<h3>Store Info</h3>
 			      			<p>{{ $store->street }}</p>
 			      			<p>{{ $store->suburb }}, {{ $store->state }} {{ $store->postcode }}</p>
 			      			<p>{{ $store->phone }}</p>
-			      			<p><a href="mailto:{{ $store->email }}"></a></p>
+			      			<p><a href="mailto:{{ $store->email }}">{{ $store->email }}</a></p>
 			      			<p><a href="{{ $store->link }}" target="_blank">Go To Site</a></p>
 			      		</div>
 			      		<div class="col-sm-6 col-xs-12">
+			      			<h3>Trading Hours</h3>
 			      			<p>{!! $store->hours !!}</p>
 			      		</div>
 			      	</div>
