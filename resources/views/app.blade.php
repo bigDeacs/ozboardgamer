a0feee92551eae3d38e969c3b6a49223292242cd
unknown <brent.deacon@AP-TMMGR01.iceenterprise.com>
2016-08-02 15:55:05 +1000

    fix app id

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -68,7 +68,7 @@
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.6&appId=1428829950709319";
+      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.6&appId=256969058009917";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
     <!-- End Facebook Plugin Manager -->    
