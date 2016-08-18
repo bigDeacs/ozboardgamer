dfd89b27dfd372a35fc5b0a2d4b54f2d2432f75c
ICEENTERPRISE\brent.deacon <brent.deacon@ourvacationcentre.com>
2016-08-18 12:55:07 +1000

    fix migration error

--- database/migrations/2016_08_18_014602_create_archetypes_table.php
+++ database/migrations/2016_08_18_014602_create_archetypes_table.php
@@ -12,7 +12,7 @@ class CreateArchetypesTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::table('archetypes', function(Blueprint $table)
+		Schema::create('archetypes', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->string('name');
