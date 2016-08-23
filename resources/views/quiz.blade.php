21ba97f63bb0711541d4c8990687dac2a91f5ced
ICEENTERPRISE\brent.deacon <brent.deacon@ourvacationcentre.com>
2016-08-23 13:47:43 +1000

    error message

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -37,6 +37,7 @@
               </div>
             </div>
             {!! Form::open(['route' => 'quizRequest', 'role' => 'form']) !!}
+              <ul class="errorMessages"></ul>
               <input type="hidden" name="quiz_id" value="{{ $quiz->id }}">
               @foreach($questions as $key => $question)
                 <div class="row setup-content" id="step-{{ $key+1 }}">
@@ -150,5 +151,51 @@
 
       $('div.setup-panel div a.btn-primary').trigger('click');
     });
+
+
+
+
+    var createAllErrors = function() {
+        var form = $( this ),
+            errorList = $( "ul.errorMessages", form );
+
+        var showAllErrorMessages = function() {
+            errorList.empty();
+
+            // Find all invalid fields within the form.
+            var invalidFields = form.find( ":invalid" ).each( function( index, node ) {
+
+                // Find the field's corresponding label
+                var label = $( "label[for=" + node.id + "] "),
+                    // Opera incorrectly does not fill the validationMessage property.
+                    message = node.validationMessage || 'Invalid value.';
+
+                errorList
+                    .show()
+                    .append( "<li><span>" + label.html() + "</span> " + message + "</li>" );
+            });
+        };
+
+        // Support Safari
+        form.on( "submit", function( event ) {
+            if ( this.checkValidity && !this.checkValidity() ) {
+                $( this ).find( ":invalid" ).first().focus();
+                event.preventDefault();
+            }
+        });
+
+        $( "input[type=submit], button:not([type=button])", form )
+            .on( "click", showAllErrorMessages);
+
+        $( "input", form ).on( "keypress", function( event ) {
+            var type = $( this ).attr( "type" );
+            if ( /date|email|month|number|search|tel|text|time|url|week/.test ( type )
+              && event.keyCode == 13 ) {
+                showAllErrorMessages();
+            }
+        });
+    };
+
+    $( "form" ).each( createAllErrors );
   </script>
 @endsection
