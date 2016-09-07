187c27c8f20250fb260a94c7592af54737340174
bigDeacs <brentdeacon23@gmail.com>
2016-09-07 11:16:15 +1000

    Updated the default route

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -292,7 +292,7 @@
 
 @section('scripts')
     <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script type="text/javascript" src="slick/slick.min.js"></script>
+    <script type="text/javascript" src="/js/slick.min.js"></script>
     <script type="text/javascript">
         var userFeed  = new Instafeed({
             get: 'user',
