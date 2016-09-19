8750ad140a84ee9010cda8be55c54e4d0534c535
bigDeacs <brentdeacon23@gmail.com>
2016-09-19 15:17:51 +1000

    Remove expansions from top 10

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
@@ -23,7 +23,7 @@ class AdminController extends Controller {
 	{
 		$this->middleware('auth');
 	}
-	
+
 	/**
 	 * Show the application dashboard to the user.
 	 *
@@ -32,7 +32,7 @@ class AdminController extends Controller {
 	public function index()
 	{
 		$latestpost = Post::orderBy('published_at', 'desc')->where('published_at', '<=', date('Y-m-d'))->first();
-		$toptengames = Game::orderBy('rating', 'desc')->take(10)->get();
+		$toptengames = Game::where('parent_id', '=', null)->orderBy('rating', 'desc')->take(10)->get();
 		$toptenstores = Store::orderBy('rating', 'desc')->take(10)->get();
         return view('home', compact('latestpost', 'toptengames', 'toptenstores'));
 	}
