ff71c262371636bccaff3c532c4fa1e8b22a2baf
bigDeacs <brentdeacon23@gmail.com>
2016-09-22 09:29:53 +1000

    Return to Disqus

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -112,7 +112,7 @@
 
                    <hr />
                    <div class="row text-center">
-                     <a href="/facebook" class="btn btn-primary"><i class="fa fa-facebook-official" aria-hidden="true"></i> Login with Facebook</a>
+                     <a href="/facebook" class="btn btn-primary"><i class="fa fa-facebook-official" aria-hidden="true"></i> Login with Facebook</a> or 
                      <a href="/google" class="btn btn-danger"><i class="fa fa-google" aria-hidden="true"></i> Login with Google</a>
                    </div>
                    <hr />
--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -132,20 +132,37 @@
 			      </div>
 			    </div>
 
-				<div class="row">
-		            <div class="col-xs-12">
-		                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-		                <!-- Footer Ad -->
-		                <ins class="adsbygoogle"
-		                     style="display:block"
-		                     data-ad-client="ca-pub-5206537313688631"
-		                     data-ad-slot="2769589305"
-		                     data-ad-format="auto"></ins>
-		                <script>
-		                (adsbygoogle = window.adsbygoogle || []).push({});
-		                </script>
-		            </div>
-		        </div>
+          @if(Session::has('name'))
+            <hr />
+            <div class="row">
+              <div class="getsocial gs-reaction-button"></div>
+              <div id="disqus_thread"></div>
+              <script>
+
+              /**
+               *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+               *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables */
+              var disqus_config = function () {
+                  this.page.url = {{ Request::url() }};  // Replace PAGE_URL with your page's canonical URL variable
+                  this.page.identifier = {{ camel_case($store->name) }}; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+              };
+              (function() { // DON'T EDIT BELOW THIS LINE
+                  var d = document, s = d.createElement('script');
+                  s.src = '//ozboardgamer.disqus.com/embed.js';
+                  s.setAttribute('data-timestamp', +new Date());
+                  (d.head || d.body).appendChild(s);
+              })();
+              </script>
+              <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+            </div>
+          @else
+            <hr />
+            <div class="row text-center">
+              <a href="/facebook" class="btn btn-primary"><i class="fa fa-facebook-official" aria-hidden="true"></i> Login with Facebook</a> or
+              <a href="/google" class="btn btn-danger"><i class="fa fa-google" aria-hidden="true"></i> Login with Google</a> to add comments
+            </div>
+          @endif
+
 			</div>
 		</div>
 	</div>
