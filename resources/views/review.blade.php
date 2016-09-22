2291d6c03bca981c2ed5aa4a3913528df605a52e
bigDeacs <brentdeacon23@gmail.com>
2016-09-22 13:04:05 +1000

    try download

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -76,48 +76,17 @@
           <hr />
           <div class="row">
             <div class="getsocial gs-reaction-button"></div>
+            <script>
 
-            <?php
-            define('DISQUS_SECRET_KEY', 'aN7OutGQ5Y8lXgdw4g4JqkmZl9CN9XAsWjn5PzONzaaRdzDBjIB2iEniwaKKkmu9');
-            define('DISQUS_PUBLIC_KEY', 'dfGV7FT4p75sDiuGmSslFTMVq5t5a2GfDXkmvJDNyaof90Dc3THzwO5cXTSH9S2C');
-
-            $data = array(
-                    "id" => Session::get('id'),
-                    "username" => Session::get('name'),
-                    "email" => Session::get('email')
-                );
-
-            function dsq_hmacsha1($data, $key) {
-                $blocksize=64;
-                $hashfunc='sha1';
-                if (strlen($key)>$blocksize)
-                    $key=pack('H*', $hashfunc($key));
-                $key=str_pad($key,$blocksize,chr(0x00));
-                $ipad=str_repeat(chr(0x36),$blocksize);
-                $opad=str_repeat(chr(0x5c),$blocksize);
-                $hmac = pack(
-                            'H*',$hashfunc(
-                                ($key^$opad).pack(
-                                    'H*',$hashfunc(
-                                        ($key^$ipad).$data
-                                    )
-                                )
-                            )
-                        );
-                return bin2hex($hmac);
-            }
-
-            $message = base64_encode(json_encode($data));
-            $timestamp = time();
-            $hmac = dsq_hmacsha1($message . ' ' . $timestamp, DISQUS_SECRET_KEY);
-            ?>
-            <script type="text/javascript">
-            var disqus_config = function() {
-                this.page.remote_auth_s3 = "<?php echo "$message $hmac $timestamp"; ?>";
-                this.page.api_key = "<?php echo DISQUS_PUBLIC_KEY; ?>";
+            /**
+             *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+             *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables */
+            var disqus_config = function () {
+                this.page.remote_auth_s3 = '{{ $data["sso"]["message"] }} {{ $data["sso"]["hmac"] }} {{ $data["sso"]["timestamp"] }}';
+                this.page.api_key = '{{ $data["sso"]["publickey"] }}';
                 this.page.url = '{{ Request::url() }}';  // Replace PAGE_URL with your page's canonical URL variable
                 this.page.identifier = '{{ camel_case($post->name) }}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-            }
+            };
             (function() { // DON'T EDIT BELOW THIS LINE
                 var d = document, s = d.createElement('script');
                 s.src = '//ozboardgamer.disqus.com/embed.js';
