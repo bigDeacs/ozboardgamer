ccf055070c6b862a58de8ee51f1008ba5821dcf1
bigDeacs <brentdeacon23@gmail.com>
2016-09-22 10:31:58 +1000

    Return to Disqus

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -360,26 +360,7 @@
         <hr />
         <div class="row">
           <div class="getsocial gs-reaction-button"></div>
-          <div id="disqus_thread"></div>
-          <script>
-
-          /**
-           *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-           *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables */
-          var disqus_config = function () {
-              this.page.remote_auth_s3 = "{{ $sso['message'] $sso['hmac'] $sso['timestamp'] }}";
-              this.page.api_key = "{{ $sso['publickey'] }}";
-              this.page.url = '{{ Request::url() }}';  // Replace PAGE_URL with your page's canonical URL variable
-              this.page.identifier = '{{ camel_case($game->name) }}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-          };
-          (function() { // DON'T EDIT BELOW THIS LINE
-              var d = document, s = d.createElement('script');
-              s.src = '//ozboardgamer.disqus.com/embed.js';
-              s.setAttribute('data-timestamp', +new Date());
-              (d.head || d.body).appendChild(s);
-          })();
-          </script>
-          <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+          
         </div>
       @else
         <hr />
