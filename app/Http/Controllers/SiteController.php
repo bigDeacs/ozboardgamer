ed8117e3488a64a7a0b48ef22e6497ccdc42e8a0
bigDeacs <brentdeacon23@gmail.com>
2016-10-07 13:01:56 +1000

    Finish base

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -879,7 +879,7 @@ class SiteController extends Controller {
 			arsort($results);
 			$i = 0;
 			foreach($results as $key => $result) {
-				$result[$i] = $key;
+				$result[] = $key;
 				$i++;
 				if($i >= 1) {
 					break;
