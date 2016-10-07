32fc4694de179cffe78ed59ccdb43da20c325323
bigDeacs <brentdeacon23@gmail.com>
2016-10-07 12:53:14 +1000

    idiot

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -877,10 +877,9 @@ class SiteController extends Controller {
 	{
 			$results = array_count_values($request['questions']);
 			arsort($results);
-			dd($results);
 			$i = 0;
-			foreach($results as $result) {
-				$result[$i] = key($result);
+			foreach($results as $key as $result) {
+				$result[$i] = $key;
 				$i++;
 				if($i >= 1) {
 					break;
