a85d4757e6add45a96f6c743a778f957e9b263c5
bigDeacs <brentdeacon23@gmail.com>
2016-12-15 13:38:05 +1000

    Hide mighty app widget on XS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -74,7 +74,7 @@
         @endunless
         <div class="row">
             <div class="col-md-9 col-xs-12">
-                <div class="col-xs-12 text-center">             
+                <div class="col-sm-12 hidden-xs text-center">             
                     <iframe src="https://www.mightyape.com.au/referrals/widget/best-sellers/wide/2865187/10231" frameborder="0" width="600" height="200" scrolling="no" style="margin-top:10px; margin-bottom:10px;"></iframe>
                 </div>
                 @unless($reviews->isEmpty())
