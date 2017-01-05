bb17d3140f8c3711089f0a195a949576b8186426
bigDeacs <brentdeacon23@gmail.com>
2017-01-05 14:58:28 +1000

    Fix offers

--- resources/views/patron.blade.php
+++ resources/views/patron.blade.php
@@ -27,7 +27,9 @@
 			    </div>
 			    <div class="row">  
              <div class="col-xs-12 col-sm-6">
-                     
+                <a href="" target="_blank" class="btn btn-block" style="background-color: #e6461a;border-color: #d04018;">
+                  <img src="/img/patreon_logo.png" class="img-responsive" alt="Patreon Logo" /> Become a Patron
+                </a>
               </div>
               <div class="col-xs-12 col-sm-6">
                    @if(isset($feature))
