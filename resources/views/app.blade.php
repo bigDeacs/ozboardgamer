2b96b2373c61c51f71bb0be7d2911dbdc458880d
bigDeacs <brentdeacon23@gmail.com>
2017-01-31 11:52:52 +1000

    Fix offers

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -332,33 +332,33 @@
               });
         });
         $('.responsive').slick({
-          dots: true,
-          infinite: false,
-          speed: 300,
-          slidesToShow: 4,
-          slidesToScroll: 4,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 5,
+          slidesToScroll: 5,
           responsive: [
             {
               breakpoint: 1024,
               settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 4,
+                slidesToScroll: 4,
                 infinite: true,
-                dots: true
               }
             },
             {
               breakpoint: 600,
               settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
               }
             },
             {
               breakpoint: 480,
               settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                infinite: true,
               }
             }
             // You can unslick at a given breakpoint now by adding:
