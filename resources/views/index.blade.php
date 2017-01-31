327301734537e5093b32d6a0da77f5316d56c3d4
bigDeacs <brentdeacon23@gmail.com>
2017-01-31 12:57:58 +1000

    Fix offers

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -258,40 +258,42 @@
 
 @section('scripts')
     <script>
-        $('.responsive').slick({
-          infinite: true,
-          speed: 500,
-          slidesToShow: 5,
-          slidesToScroll: 5,
-          responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 4,
-                slidesToScroll: 4,
-                infinite: true,
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
+        $(document).ready(function(){
+          $('.responsive').slick({
+              infinite: true,
+              speed: 500,
+              slidesToShow: 5,
+              slidesToScroll: 5,
+              responsive: [
+                {
+                  breakpoint: 1024,
+                  settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    infinite: true,
+                  }
+                },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                  }
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                  }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+              ]
+            });
         });
     </script>
 @endsection
