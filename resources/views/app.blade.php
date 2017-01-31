8974f6180c4380faaa70f8cd96b7313d57284b12
bigDeacs <brentdeacon23@gmail.com>
2017-01-31 10:34:57 +1000

    Fix offers

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -38,9 +38,6 @@
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="https://ozboardgamer.com/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/instantsearch.js/1/instantsearch.min.css">
-  <script type="text/javascript">
-    (function() { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = '//api.at.getsocial.io/widget/v1/gs_async.js?id=8ba0b1'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })();
-  </script>
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -364,5 +361,6 @@
         });
     </script>
     @yield('scripts')
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-588fdb0471f79775"></script> 
   </body>
 </html>
