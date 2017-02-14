9b60333c2ee30a434d6861307333a394170d993b
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-02-14 16:01:37 +1000

    Rename CruiseShips.cshtml to Ships.cshtml

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -201,22 +201,24 @@
             <div class="col-md-3 col-sm-4 col-xs-12">       
 				@unless($products->isEmpty())
 					<h3>Featured Product</h3>
-					@foreach($products as $product)						
-						<a href="{!! $product->slug !!}" target="_blank">
-							<img src="{{ $product->thumb1x }}" srcset="{{ $product->thumb1x }} 1x, {{ $product->thumb2x }} 2x" class="img-responsive" />
-						</a>
-						<p class="text-center">
-							<strong><a href="{!! $product->slug !!}" target="_blank">{!! str_limit(strip_tags($product->name), $limit = 50, $end = '...') !!}</a></strong><br />
-							@if($product->sale > 0)
-								<strong>${!! $product->saleDisplay !!}</strong><br />
-								<s><small>${!! $product->priceDisplay !!}</small></s>
-							@else
-								<strong>${!! $product->priceDisplay !!}</strong>
-							@endif
-						</p>
-						<p class="text-center">
-							<a class="btn btn-danger" href="{!! $product->slug !!}" target="_blank">Buy now <i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
-						</p>
+					@foreach($products as $product)	
+						<div class="text-center">					
+							<a href="{!! $product->slug !!}" target="_blank">
+								<img src="{{ $product->thumb1x }}" srcset="{{ $product->thumb1x }} 1x, {{ $product->thumb2x }} 2x" class="img-responsive" />
+							</a>
+							<p class="text-center">
+								<strong><a href="{!! $product->slug !!}" target="_blank">{!! str_limit(strip_tags($product->name), $limit = 50, $end = '...') !!}</a></strong><br />
+								@if($product->sale > 0)
+									<strong>${!! $product->saleDisplay !!}</strong><br />
+									<s><small>${!! $product->priceDisplay !!}</small></s>
+								@else
+									<strong>${!! $product->priceDisplay !!}</strong>
+								@endif
+							</p>
+							<p class="text-center">
+								<a class="btn btn-danger" href="{!! $product->slug !!}" target="_blank">Buy now <i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+							</p>
+						</div>
 					@endforeach
 					<hr class="hidden-xs" />
                 @endunless
