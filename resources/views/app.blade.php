b1f44e52799df0192f99d1449e93021b136ac5ef
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-02-20 14:10:20 +1000

    Fixed Video check in Cruiseline page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,7 +26,7 @@
     @yield('head')
     <script type="text/javascript">if(window.location.hash == '#_=_' || window.location.hash == '#') { window.location.hash = ''; history.pushState('', document.title, window.location.pathname); }</script>
 	<link href="https://ozboardgamer.com/css/fonts.css" rel="stylesheet">
-    <link href="https://assets.ozboardgamer.com/css/style.min.css?v=35" rel="stylesheet">
+    <link href="https://assets.ozboardgamer.com/css/style.min.css?v=36" rel="stylesheet">
 	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="https://assets.ozboardgamer.com/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 	<!-- Fonts -->
@@ -299,7 +299,7 @@
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">×</button>
 				<br><br>
 				<form id="search" action="#" method="post" style="width: 100%;">
-					<input type="text" name="search-terms" id="search-terms" class="form-control" placeholder="Find your next favourite game..." style="height: 75px;font-size: 25px;" onfocus="if(this.value == 'value') { this.value = ''; }">
+					<input type="text" name="search-terms" id="search-terms" class="form-control" placeholder="Find your next game..." style="height: 70px;font-size: 20px;" onblur="this.value='';">
 				</form>
 			</div>
 		</div>
