1b92db0ead745e0cde7f30bb7a4b7a5e212a37cc
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-02-20 13:44:28 +1000

    Fixed Video check in Cruiseline page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,7 +26,7 @@
     @yield('head')
     <script type="text/javascript">if(window.location.hash == '#_=_' || window.location.hash == '#') { window.location.hash = ''; history.pushState('', document.title, window.location.pathname); }</script>
 	<link href="https://ozboardgamer.com/css/fonts.css" rel="stylesheet">
-    <link href="https://assets.ozboardgamer.com/css/style.min.css?v=32" rel="stylesheet">
+    <link href="https://assets.ozboardgamer.com/css/style.min.css?v=33" rel="stylesheet">
 	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="https://assets.ozboardgamer.com/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 	<!-- Fonts -->
@@ -87,8 +87,8 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand topnav" href="/" title="Click to go to home page" style="padding: 15px 0;">
-						<img src="https://assets.ozboardgamer.com/img/logo.png" class="img-responsive" height="75" width="auto" alt="OzBoardGamer Logo" />
+					<a class="navbar-brand topnav" href="/" title="Click to go to home page">
+						<img src="https://assets.ozboardgamer.com/img/logo.png" class="img-responsive" height="95" width="auto" alt="OzBoardGamer Logo" />
 					</a>
 				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
@@ -290,10 +290,10 @@
 				</div>
 			</div>
 		</footer>
-	</div>
+	</div>	
 	
 <div class="modal fade" id="searchWrapper" tabindex="-1" role="dialog" aria-labelledby="searchWrapperLabel" aria-hidden="true">
-	 <div class="container" style="margin-top: 50px;">
+	 <div class="container">
 		<div class="row">
 			<div class="col-sm-8 col-sm-offset-2 col-xs-12 text-center">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">×</button>
