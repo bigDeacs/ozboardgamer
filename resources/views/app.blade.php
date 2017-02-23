e55bafc348bb1c1dd36a9d8faf45ab960da0d393
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-02-23 13:33:44 +1000

    Add get social

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -53,12 +53,12 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+	<script type='text/javascript' src='//platform-api.sharethis.com/js/sharethis.js#property=58ae5064f07e8a0012dd628e&product=inline-share-buttons'></script>
   </head>
 
   <body>
 	 <!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PR2VZT5"
-	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PR2VZT5" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	<!-- End Google Tag Manager (noscript) -->
 	
     <!-- Facebook Plugin Manager -->
