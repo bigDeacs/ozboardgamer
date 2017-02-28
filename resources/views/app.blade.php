b800b76f983e708189ecedc744ba6bf8a6e948cd
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-02-28 11:59:41 +1000

    Update list of Destinations based on OVS

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,9 +32,9 @@
     @yield('meta')
     @yield('head')
     <script type="text/javascript">if(window.location.hash == '#_=_' || window.location.hash == '#') { window.location.hash = ''; history.pushState('', document.title, window.location.pathname); }</script>
-    <link href="https://css.ozboardgamer.com/css/style.min.css?v=42" rel="stylesheet">
-	<link href="https://ozboardgamer.com/css/fonts.css" rel="stylesheet">
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link href="https://css.ozboardgamer.com/css/style.min.css?v=43" rel="stylesheet">
+	<!--<link href="https://ozboardgamer.com/css/fonts.css" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>-->
 	<link rel="stylesheet" href="https://ozboardgamer.com/css/font-awesome.min.css">
 	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="https://css.ozboardgamer.com/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
