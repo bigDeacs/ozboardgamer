133b2f157d6420a12825c840532a97de8219f9a0
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-03-15 09:41:08 +1000

    Test premium

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,6 +10,9 @@
 @endsection
 
 @section('content')
+	@if(Session::has('name') || date("F d, Y") >= date($post->published_at, strtotime("+ 2 day"))
+		Premium
+	@endif
 	<div class="breadcrumb-holder hidden-lg hidden-md hidden-sm">
 		<div class="container">
 			<ol class="breadcrumb breadcrumb-arrow">
