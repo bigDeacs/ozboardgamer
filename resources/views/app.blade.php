b0c57b939abcd4b0f061281e922b644a24c61d20
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-03-28 09:35:09 +1000

    Add Optomize

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -56,6 +56,17 @@
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));</script>
 	<!-- End Facebook Plugin -->
+	<!-- Optimize Plugin -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-75788516-1', 'auto');
+	  ga('require', 'GTM-MFDVG9R');
+	  ga('send', 'pageview');
+	</script>
+	<!-- End Optimize Plugin -->
   </head>
   <body>	
 	<div id="wrap">
