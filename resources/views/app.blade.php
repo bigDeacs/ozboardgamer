0b27944f49021ccdd656241542efc3669e6cee58
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-04-20 16:19:59 +1000

    Fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -49,7 +49,7 @@
     @yield('meta')
     @yield('head')
     <script type="text/javascript">if(window.location.hash == '#_=_' || window.location.hash == '#') { window.location.hash = ''; history.pushState('', document.title, window.location.pathname); }</script>
-    <link href="https://css.ozboardgamer.com/css/style.min.css?v=58" rel="stylesheet">
+    <link href="https://css.ozboardgamer.com/css/style.min.css?v=59" rel="stylesheet">
 	<!--<link href="https://ozboardgamer.com/css/fonts.css" rel="stylesheet">
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>-->
 	<link rel="stylesheet" href="https://ozboardgamer.com/css/font-awesome.min.css">
