615f53e639561458a457df895be7f4fce5b65a11
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-04-21 15:02:26 +1000

    Fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -115,7 +115,7 @@
 					<div class="navbar-right" style="margin: 32px 0;">
 						<div class="navbar-right">
 							<ul class="nav navbar-nav">                            
-								<li><a type="button" data-toggle="modal" data-target="#searchWrapper" style="padding: 10px 10px;cursor: pointer;" title="Search Games"><i class="fa fa-search" aria-hidden="true" style="color: #008751;font-size: 20px;"></i></a></li>
+								<li><a type="button" data-toggle="modal" data-target="#searchWrapper" style="padding: 10px 10px;cursor: pointer;" title="Search Games" onclick="focus()"><i class="fa fa-search" aria-hidden="true" style="color: #008751;font-size: 20px;"></i></a></li>
 								<li class="dropdown">
 								  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Games <span class="caret"></span></a>
 								  <ul class="dropdown-menu">
@@ -416,8 +416,10 @@
 					}, 800);
 					return false;
 				});   	
-				if (!/iPad|iPhone|iPod/g.test(navigator.userAgent)) {
-					document.getElementById("header-search").focus();
+				function focus() {
+					if (!/iPad|iPhone|iPod/g.test(navigator.userAgent)) {
+						document.getElementById("header-search").focus();
+					}
 				}
 			});
 		</script>
@@ -438,8 +440,10 @@
 					}, 800);
 					return false;
 				});   	
-				if (!/iPad|iPhone|iPod/g.test(navigator.userAgent)) {
-					document.getElementById("header-search").focus();
+				function focus() {
+					if (!/iPad|iPhone|iPod/g.test(navigator.userAgent)) {
+						document.getElementById("header-search").focus();
+					}
 				}
 				// Cookie Set
 				var my_cookie = $.cookie($('.modal-check').attr('name'));
