7d294b264bbf7692eebd4b0f709550d59f9358be
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-05-11 14:07:17 +1000

    Add brand

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -41,7 +41,7 @@
 					@unless($post->image == null)
 						<div class="row">
 						  <div class="col-sm-12 hidden-xs">
-							<img src="{{ $post->user->image }}" alt="{{ $post->user->name }}" class="img-circle img-shadow" width="100px" height="auto" style="position: absolute;bottom: 10px;left: 25px;" data-toggle="tooltip" data-placement="right" title="{!! strip_tags($post->user->description) !!}">
+							<img src="{{ $post->user->image }}" alt="{{ $post->user->name }}" class="img-circle img-shadow" width="100px" height="auto" style="position: absolute;bottom: 10px;left: 25px;" data-toggle="tooltip" data-placement="right" title="{{ strip_tags($post->user->description) }}">
 							<h1>{{ $post->name }}</h1>
 							<div class="img-container">
 								<div class="fill" style="background-image:url('https://img.ozboardgamer.com/{{ $post->image }}');" itemprop="image"></div>
@@ -94,7 +94,7 @@
 					@unless($post->image == null)
 						<div class="row">
 						  <div class="col-sm-12 hidden-xs">
-						  <img src="{{ $post->user->image }}" alt="{{ $post->user->name }}" class="img-circle img-shadow" width="100px" height="auto" style="position: absolute;bottom: 10px;left: 25px;" data-toggle="tooltip" data-placement="right" title="{!! strip_tags($post->user->description) !!}">
+						  <img src="{{ $post->user->image }}" alt="{{ $post->user->name }}" class="img-circle img-shadow" width="100px" height="auto" style="position: absolute;bottom: 10px;left: 25px;" data-toggle="tooltip" data-placement="right" title="{{ strip_tags($post->user->description) }}">
 							<div class="img-container">
 								<div class="fill" style="background-image:url('https://img.ozboardgamer.com/{{ $post->image }}');"></div>
 							</div>
