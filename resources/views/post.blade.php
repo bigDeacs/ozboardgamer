3b1f44b8cd66e097d6abb149d30a492a7af96b3e
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-05-11 13:11:11 +1000

    Add brand

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -35,7 +35,7 @@
 					@unless($post->image == null)
 						<div class="row">
 						  <div class="col-sm-12 hidden-xs">
-							<img src="{{ $post->user->image }}" alt="{{ $post->user->name }}" class="img-circle img-shadow" width="100px" height="auto" style="position: absolute;bottom: 10px;left: 25px;" data-toggle="tooltip" data-placement="right" title="Tooltip on right">
+							<img src="{{ $post->user->image }}" alt="{{ $post->user->name }}" class="img-circle img-shadow" width="100px" height="auto" style="position: absolute;bottom: 10px;left: 25px;" data-toggle="tooltip" data-placement="right" title="{{ $post->user->description }}">
 							<h1>{{ $post->name }}</h1>
 							<div class="img-container">
 								<div class="fill" style="background-image:url('https://img.ozboardgamer.com/{{ $post->image }}');" itemprop="image"></div>
@@ -88,7 +88,7 @@
 					@unless($post->image == null)
 						<div class="row">
 						  <div class="col-sm-12 hidden-xs">
-						  <img src="{{ $post->user->image }}" alt="{{ $post->user->name }}" class="img-circle img-shadow" width="100px" height="auto" style="position: absolute;bottom: 10px;left: 25px;" data-toggle="tooltip" data-placement="right" title="Tooltip on right">
+						  <img src="{{ $post->user->image }}" alt="{{ $post->user->name }}" class="img-circle img-shadow" width="100px" height="auto" style="position: absolute;bottom: 10px;left: 25px;" data-toggle="tooltip" data-placement="right" title="{{ $post->user->description }}">
 							<div class="img-container">
 								<div class="fill" style="background-image:url('https://img.ozboardgamer.com/{{ $post->image }}');"></div>
 							</div>
--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -35,6 +35,7 @@
 					@unless($post->image == null)
 						<div class="row">
 						  <div class="col-sm-12 hidden-xs">
+							<img src="{{ $post->user->image }}" alt="{{ $post->user->name }}" class="img-circle img-shadow" width="100px" height="auto" style="position: absolute;bottom: 10px;left: 25px;" data-toggle="tooltip" data-placement="right" title="{{ $post->user->description }}">
 							<div class="img-container">
 								<div class="fill" style="background-image:url('https://img.ozboardgamer.com/{{ $post->image }}');" itemprop="image"></div>
 							</div>
@@ -86,6 +87,7 @@
 					@unless($post->image == null)
 						<div class="row">
 						  <div class="col-sm-12 hidden-xs">
+							<img src="{{ $post->user->image }}" alt="{{ $post->user->name }}" class="img-circle img-shadow" width="100px" height="auto" style="position: absolute;bottom: 10px;left: 25px;" data-toggle="tooltip" data-placement="right" title="{{ $post->user->description }}">
 							<div class="img-container">
 								<div class="fill" style="background-image:url('https://img.ozboardgamer.com/{{ $post->image }}');" itemprop="image"></div>
 							</div>
@@ -201,6 +203,9 @@
 	<script>
     $(document).ready(function() {
             $("#child").css("height",$("#parent").height() - 50);
+			$(function () {
+			  $('[data-toggle="tooltip"]').tooltip()
+			});
      });
   </script>
   <script>
