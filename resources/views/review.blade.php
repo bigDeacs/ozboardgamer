366b970c585ba6a6e8c1890dc7e82d0b06fe112d
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-05-18 15:28:20 +1000

    Update

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -195,7 +195,7 @@
 							})();
 							</script>
 							<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-							<div id="disqus_thread"></div>
+							<div class="col-xs-12" id="disqus_thread"></div>
 						</div>
 					  </div>
 					@else
