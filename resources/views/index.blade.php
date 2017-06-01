e0ed5e7915aaccac7fc16db494e4e6f3b2086996
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-06-01 11:23:46 +1000

    Fix social sharing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -228,11 +228,6 @@
 													</a>	
 												@endif											
 											</div>
-											@unless(Session::has('name') == false && date('F d, Y', strtotime("now")) == date('F d, Y', strtotime($top10->published_at)))											
-												<div class="fb-share-button" data-href="https://ozboardgamer.com/top10s/{{ $top10->slug }}" data-layout="button" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https://ozboardgamer.com/top10s/{{ $top10->slug }}">Share</a></div>
-												<a class="twitter-share-button" href="https://ozboardgamer.com/top10s/{{ $top10->slug }}">Tweet</a>
-												<div class="g-plusone" data-size="tall" data-annotation="none" data-href="https://ozboardgamer.com/top10s/{{ $top10->slug }}"></div>
-											@endunless
 										</div>
 										<div class="col-sm-8 col-md-9 col-xs-12">
 									@endif
