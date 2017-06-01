f9a4a86ee3b7f5134fc06d31380f1eebd4fe8397
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-06-01 11:03:14 +1000

    Fix social sharing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -74,6 +74,24 @@
 	}(document, 'script', 'facebook-jssdk'));</script>
 	<!-- End Facebook Plugin -->
 	<script src="https://apis.google.com/js/platform.js" async defer></script>
+	<!-- Twitter Plugin -->
+	<script>window.twttr = (function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0],
+		t = window.twttr || {};
+	  if (d.getElementById(id)) return t;
+	  js = d.createElement(s);
+	  js.id = id;
+	  js.src = "https://platform.twitter.com/widgets.js";
+	  fjs.parentNode.insertBefore(js, fjs);
+
+	  t._e = [];
+	  t.ready = function(f) {
+		t._e.push(f);
+	  };
+
+	  return t;
+	}(document, "script", "twitter-wjs"));</script>
+	<!-- End Twitter Plugin -->
 	<!--  Hiding Script -->
 	<style>.async-hide { opacity: 0 !important} </style>
 	<script data-cfasync="false">(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
