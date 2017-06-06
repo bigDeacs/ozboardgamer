999e00c2f83057a647e1af4a0422c81370ef11a1
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-06-06 12:58:55 +1000

    Add link to facebook

--- resources/views/appAdmin.blade.php
+++ resources/views/appAdmin.blade.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9&appId=1535960793102155";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
-	
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -133,11 +133,6 @@
 							</div>
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-xs-12 col-sm-6">
-							<div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
-						</div>
-					</div>
 				</div>
 			</div>
 		</div>
