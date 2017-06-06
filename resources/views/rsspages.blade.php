4f4dd1a56f936d16dd2f329a28af4a980c1ee2e6
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-06-06 16:33:34 +1000

    Add Feed service

--- resources/views/rsspages.blade.php
+++ resources/views/rsspages.blade.php
@@ -2,8 +2,10 @@
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/">
     <channel>
         <title>{!! $channel['title'] !!}</title>
-        <link>https://ozboardgamer.com</link>
-        <description><![CDATA[{!! $channel['description'] !!}]]></description>
+        <link>{{ $channel['rssLink'] }}</link>
+        <description>
+			<![CDATA[{!! $channel['description'] !!}]]>
+		</description>
         <language>en-us</language>
         <lastBuildDate>{{ $channel['pubdate'] }}</lastBuildDate>
         @foreach($items as $item)
@@ -15,8 +17,33 @@
 				<author>{!! $item['author'] !!}</author>
 				<description><![CDATA[{!! $item['description'] !!}]]></description>
 				@if (!empty($item['content']))
-					<content:encoded><![CDATA[{!! $item['content'] !!}]]></content:encoded>
-				@endif			
+					<content:encoded>
+						<![CDATA[
+							<!doctype html>
+							<html lang="en" prefix="op: http://media.facebook.com/op#">
+							  <head>
+								<meta charset="utf-8">
+								<link rel="canonical" href="{{ $item['link'] }}">
+								<meta property="op:markup_version" content="v1.0">
+							  </head>
+							  <body>
+								<article>
+									<header>
+									  <figure>
+										<img src="{{ $item['media:content']['url'] }}" />
+									  </figure>
+									  <h1>{{ $item['title'] }}</h1>
+									  <address>
+										<a>{!! $item['author'] !!}</a>
+									  </address>
+									</header>
+									{!! $item['content'] !!}
+								</article>
+							  </body>
+							</html>
+						]]>
+					</content:encoded>
+				@endif		
 			</item>
         @endforeach
     </channel>
