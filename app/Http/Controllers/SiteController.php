2f17e117236913f9a4b00da51df1848aca6ea0eb
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-06-06 13:17:28 +1000

    Add Feed service

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -655,7 +655,7 @@ class SiteController extends Controller {
 			   foreach ($posts as $post)
 			   {
 				   // set item's title, author, url, pubdate, description, content, enclosure (optional)*
-				   $feed->add($post->title, $post->author, URL::to($post->slug), $post->created, $post->description, $post->content);
+				   $feed->add($post->name, $post->user->name, "https://ozboardgamer.com/".$post->category->slug."/".$post->slug, $post->date('F d, Y', strtotime($post->published_at)), str_limit(strip_tags($post->description), $limit = 250, $end = '...'), $post->description);
 			   }
 
 			}
