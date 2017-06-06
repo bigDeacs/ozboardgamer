1652cb01b85e195eb5505f9107ef001e1476e82b
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-06-06 14:09:17 +1000

    Render RSS

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -665,7 +665,7 @@ class SiteController extends Controller {
 			// first param is the feed format
 			// optional: second param is cache duration (value of 0 turns off caching)
 			// optional: you can set custom cache key with 3rd param as string
-			return $feed->render('rss');
+			return $feed->render();
 			
 
 			// to return your feed as a string set second param to -1
