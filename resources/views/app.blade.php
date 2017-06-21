6c2542c094fb3881b43409f2512340a89f9132e8
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-06-21 16:18:33 +1000

    Fix deal page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-THQLSV');</script>
+	<!-- End Google Tag Manager -->
 	<!-- Instant Articles -->
 	<meta property="fb:pages" content="489804094539472" />
     <meta charset="utf-8">
@@ -66,54 +73,16 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
-	<!-- Facebook Plugin -->
-	<div id="fb-root"></div>
-	<script data-cfasync="false">(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=1586488208343901";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
-	<!-- End Facebook Plugin -->
 	<script src="https://apis.google.com/js/platform.js" async defer></script>
-	<!-- Twitter Plugin -->
-	<script>window.twttr = (function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0],
-		t = window.twttr || {};
-	  if (d.getElementById(id)) return t;
-	  js = d.createElement(s);
-	  js.id = id;
-	  js.src = "https://platform.twitter.com/widgets.js";
-	  fjs.parentNode.insertBefore(js, fjs);
-
-	  t._e = [];
-	  t.ready = function(f) {
-		t._e.push(f);
-	  };
-
-	  return t;
-	}(document, "script", "twitter-wjs"));</script>
-	<!-- End Twitter Plugin -->
+	
 	<!--  Hiding Script -->
 	<style>.async-hide { opacity: 0 !important} </style>
 	<script data-cfasync="false">(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
 	h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
 	(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
 	})(window,document.documentElement,'async-hide','dataLayer',4000,
-	{'GTM-MFDVG9R':true});</script>
+	{'GTM-THQLSV':true});</script>
 	<!-- End Hiding Script -->
-	<!-- Optimize + Analytics Plugin -->
-	<script data-cfasync="false">
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-	  ga('create', 'UA-75788516-1', 'auto');
-	  ga('require', 'GTM-MFDVG9R');
-	  ga('send', 'pageview');
-	</script>
-	<!-- End Optimize + Analytics Plugin -->
   </head>
   <body>	
 	<div id="wrap">
@@ -422,14 +391,5 @@
 		</script>
 	@endif
     @yield('scripts')
-	<!-- Google Tag Manager -->
-	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-M29GVVK"
-	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-M29GVVK');</script>
-	<!-- End Google Tag Manager -->
   </body>
 </html>
