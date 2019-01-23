760df309d5fb376651a807e4d8e7e8321b71f33f
Brent Deacon <brent.deacon@xtreme.com.au>
2019-01-23 14:03:44 +1000

    Add Filter for products

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -20,7 +20,7 @@
 	</div>
 	<div class="container">
 		<div class="row">
-	      	<div class="col-sm-5 col-xs-12">
+	      	<div class="col-sm-9 col-xs-12">
 	      		<h1>Buy Online</h1>
 	      	</div>
             <div class="col-sm-3 col-xs-12">
