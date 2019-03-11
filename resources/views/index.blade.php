d8df88844054b0fe3e093b008abfcb89cc8f3d47
Brent Deacon <brent.deacon@xtreme.com.au>
2019-03-11 14:10:35 +1000

    Fix Header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -117,14 +117,16 @@
 						@foreach($reviews as $key => $review)
 							<meta itemprop="name" content = "{{ $review->games()->first()->name }}">
 							@if ($key == 0)
-								<div class="col-12 col-lg-6 px-3">
+								<div class="col-12 col-lg-6 px-2 pr-4">
 							@elseif ($key == 1)
 								<div class="col-12 col-lg-6 px-3">
 									<div class="row no-gutters">
 							@endif
-								@unless($key == 0)
-									<div class="col-6 pb-3 px-2">
-								@endunless
+								@if($key == 1 || $key = 2)
+									<div class="col-6 pb-4 px-4">
+								@elseif($key == 3 || $key = 4)
+									<div class="col-6 pt-4 px-4">
+								@endif
 										<div class="card border-0 rounded-0 text-white overflow zoom" itemscope itemtype="http://schema.org/Game">
 											<!--thumbnail-->
 											<div class="position-relative">
